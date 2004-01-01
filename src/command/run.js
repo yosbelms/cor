534a1afb08df10d1512baa0748bf0feae8de6385
Yosbel Marin <yosbelms@gmail.com>
2004-01-01 01:16:59 -0500

    improve run command

--- src/command/run.js
+++ src/command/run.js
@@ -7,8 +7,47 @@ require('../loader/node.js');
 var
 confFilename,
 entryPath,
-loader = cor.loader = new cor.Loader();
 
+Module   = require('module'),
+fs       = require('fs'),
+nodePath = require('path'),
+path     = cor.path,
+loader   = cor.loader = new cor.Loader(),
+origRequire = Module.prototype.require;
+
+Module.prototype.require = function(filename) {
+    var
+    plugin, mod,
+    ext = nodePath.extname(filename);
+    
+    if (ext === '') {
+        ext = nodePath.extname(this.filename);
+        mod = loader.moduleCache[path.sanitize(nodePath.resolve(filename + ext))];
+    }
+    
+    if (mod) {
+        return mod.getExports();
+    }
+    else {
+        return origRequire.apply(this, arguments);    
+    }
+}
+
+cor.Program.prototype.getExports = function(parent) {    
+    var js,
+    mod = new Module(this.filename, parent);
+    
+    mod.filename = this.filename;
+    mod.paths    = Module._nodeModulePaths(nodePath.dirname(this.filename));
+    mod.loaded   = true;
+    mod._contextLoad = true;
+        
+    js = this.toJs();
+    
+    mod._compile(js.src, this.filename);
+    
+    return mod.exports || {};
+}
 
 function run() {
     loader.setEntry(path.sanitize(entryPath || ''), path.sanitize(confFilename || ''));    
