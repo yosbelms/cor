0a438fad910c2add36b35c1c1085b0a7877c7c49
Yosbel Marin <yosbelms@gmail.com>
2004-01-01 03:15:19 -0500

    improved the header stub for build command

--- src/crl.js
+++ src/crl.js
@@ -150,7 +150,7 @@ o.crl_keys = function(object) {
 
     if (object instanceof Array) {
         for (i = 0, len = object.length; i < len; i++) {
-            keys.push(i)
+            keys.push(i);
         }
     }
     else {
